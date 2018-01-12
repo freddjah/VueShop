a5b21dcb6c30565e8d502711ec90d867a7f5bcc2
unknown <fredrik.ekloof@gmail.com>
2018-01-12 15:15:35 +0100

    validating form in checkout

--- client/src/components/checkout_components/CustomerInformation.vue
+++ client/src/components/checkout_components/CustomerInformation.vue
@@ -7,6 +7,9 @@
     <v-form v-model="validation.formIsValid" class="px-4 py-2">
       <v-text-field label="Name" v-model="customerInformation.name" :rules="validation.nameRules" required></v-text-field>
       <v-text-field label="E-mail" v-model="customerInformation.email" :rules="validation.emailRules" required></v-text-field>
+      <v-text-field label="Street adress" v-model="customerInformation.street" :rules="validation.streetRules" required></v-text-field>
+      <v-text-field label="Zip code" v-model="customerInformation.zipcode" :rules="validation.zipCodeRules" required></v-text-field>
+      <v-text-field label="City" v-model="customerInformation.city" :rules="validation.cityRules" required></v-text-field>
       <v-btn flat color="success">Create order</v-btn>
     </v-form>
   </v-card>
@@ -27,12 +30,26 @@ export default {
       validation: {
         formIsValid: false,
         nameRules: [
-          (v) => !!v || 'Name is required'
+          (v) => !!v || 'Name is required',
+          (v) => /^[a-zA-Z ]{2,30}$/.test(v) || 'Name must be valid. Maximum of 30 characters.'
         ],
         emailRules: [
           (v) => !!v || 'E-mail is required',
           // eslint-disable-next-line
           (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        ],
+        streetRules: [
+          (v) => !!v || 'Street adress is required',
+          (v) => /^\w+(\s\w+)*$/.test(v) || 'Street adress must be valid'
+        ],
+        zipCodeRules: [
+          (v) => !!v || 'Zip code is required',
+          (v) => v.length <= 6 || 'Too many characters',
+          (v) => /^\d{3}\s?\d{2}$/.test(v) || 'Zip code must be valid. Example: 123 45'
+        ],
+        cityRules: [
+          (v) => !!v || 'Street adress is required',
+          (v) => /^\w+(\s\w+)*$/.test(v) || 'Street adress must be valid'
         ]
       }
     }
